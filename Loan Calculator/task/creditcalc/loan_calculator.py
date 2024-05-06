e54dd580750367fb36b4279f8c997759c46d19b5
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-05-07 00:46:37 +0200

    Stage 2/4

--- Loan Calculator/task/creditcalc/loan_calculator.py
+++ Loan Calculator/task/creditcalc/loan_calculator.py
@@ -1,12 +1,28 @@
-loan_principal = 'Loan principal: 1000'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
-final_output = 'The loan has been repaid!'
+import math
 
-# write your code here
-print(loan_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+
+def menu():
+    loan_amount = int(input("Enter the load principal:\n"))
+    user_selection = input("What do you want to calculate?\ntype 'm' - for the number of monthly payments,\n"
+                           "type 'p' - for the monthly payment:\n")
+    if user_selection == "m":
+        number_of_monthly_payments(loan_amount)
+    else:
+        monthly_payment(loan_amount)
+
+
+def number_of_monthly_payments(loan_amount):
+    monthly_payment_amount = int(input("Enter the monthly payment:\n"))
+    total_months = (loan_amount + monthly_payment_amount - 1) // monthly_payment_amount
+    print(f"It will take {total_months}", "months" if total_months > 1 else "month", "to repay the loan")
+
+
+def monthly_payment(loan_amount):
+    number_of_months = int(input("Enter the number of months:\n"))
+    payment = math.ceil(loan_amount / number_of_months)
+    payment_last = loan_amount - (number_of_months - 1) * payment
+    print(f"Your monthly payment = {payment}",
+          f"and the last payment = {payment_last}." if payment_last != payment else '')
+
+
+menu()
